92bf877f878bfd9e95238b7434e003624a0be1a9
pringgojs <odyinggo@gmail.com>
2019-06-07 05:54:02 +0700

    add api serch price and customer

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Http\Request;
 */
 
 // API
+Route::get('slot/{slot}', 'ApiController@findSlot');
+Route::get('customer/{id}', 'ApiController@findCustomer');
 Route::get('clients', 'ApiController@getClient');
 Route::post('transaction', 'ApiController@transaction');
 Route::post('customer', 'ApiController@customer');
