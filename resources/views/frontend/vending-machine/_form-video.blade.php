7ff7e28f0422db000aa0293a131d25732260fea8
pringgojs <odyinggo@gmail.com>
2019-08-16 13:46:32 +0700

    update width

--- resources/views/frontend/vending-machine/_form-video.blade.php
+++ resources/views/frontend/vending-machine/_form-video.blade.php
@@ -21,7 +21,7 @@
                     $url = explode('=', $vending_machine->video);
                     ?>
                     @if(count($url) > 1)
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/{{$url[1]}}" frameborder="0"
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/{{$url[1]}}" frameborder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     @endif
                 @endif
