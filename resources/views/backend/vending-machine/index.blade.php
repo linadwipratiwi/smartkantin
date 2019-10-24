f55578dd7a738320b0041f7a9d65ea02332c8e16
pringgojs <odyinggo@gmail.com>
2019-10-24 22:11:21 +0700

    handler bug

--- resources/views/backend/vending-machine/index.blade.php
+++ resources/views/backend/vending-machine/index.blade.php
@@ -60,8 +60,8 @@
                                             <td>{{$vending_machine->production_year}}</td>
                                             <td>{{$vending_machine->location}}</td>
                                             <td>{{$vending_machine->ip}}</td>
-                                            <td>{{$vending_machine->firmware->name}}</td>
-                                            <td>{{$vending_machine->ui->name}}</td>
+                                            <td>{{$vending_machine->firmware ? $vending_machine->firmware->name : '-'}}</td>
+                                            <td>{{$vending_machine->ui ? $vending_machine->ui->name : '-'}}</td>
                                             <td>
                                                 <a href="{{url('vending-machine/'.$vending_machine->id.'/edit')}}" data-toggle="tooltip" data-original-title="Edit">
                                                     <button class="btn btn-default btn-icon-anim btn-square btn-sm"><i class="fa fa-pencil"></i></button>
