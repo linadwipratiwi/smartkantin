f1a582e2a6d06d39599b1ce6860cc5db9bdc4ba6
pringgojs <odyinggo@gmail.com>
2019-12-01 12:53:09 +0700

    fix pos add to cart

--- app/Http/Controllers/Frontend/PosController.php
+++ app/Http/Controllers/Frontend/PosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Models\Temp;
 use App\Models\Category;
+use App\Helpers\PosHelper;
 use Illuminate\Http\Request;
 use App\Models\VendingMachine;
 use App\Helpers\TempDataHelper;
@@ -14,43 +15,75 @@ class PosController extends Controller
 {
     public function index()
     {
+        $temp_key = PosHelper::getTempKey();
+
+        $data = TempDataHelper::get($temp_key, auth()->user()->id);
+        $total_item = count($data);
+        $total_price = 0;
+        $cart = [];
+        foreach ($data as $key => $value) {
+            $quantity = $value['quantity'];
+            $price = $quantity * $value['selling_price_item'];
+            $total_price += $price;
+        }
+        $cart['total_item'] = $total_item;
+        $cart['total_price'] = format_price($total_price);
+        
         $view = view('frontend.c.pos.index');
         $view->list_stand = VendingMachine::clientId(customer()->client->id)->stand()->get();
-        $view->list_food = VendingMachineSlot::whereNotNull('category_id')->get();
+        $view->stand = VendingMachine::clientId(customer()->client->id)->stand()->first();
         $view->categories = Category::food()->get();
-
+        $view->cart = $cart;
         return $view;
     }
 
     public function _addToCart($id)
     {
-        $temp_key = 'customer.basket.'.customer()->id;
+        $is_remove = \Input::get('is_remove');
+        $temp_key = PosHelper::getTempKey();
 
         $search = TempDataHelper::searchKeyValue($temp_key, auth()->user()->id, ['item_id'], [$id]);
         $item = VendingMachineSlot::findOrFail($id);
 
         $quantity = 1;
         if ($search) {
-            $quantity = $search['quantity'] + 1;
+            $quantity = $is_remove ? $search['quantity'] - 1 : $search['quantity'] + 1;
         }
         $data = [
             'item_id' => $id,
-            'name' => $item->food_name,
+            'name' => $item->food->name,
+            'selling_price_item' => $item->food->selling_price_vending_machine,
             'quantity' => $quantity,
             'photo' => asset($item->photo),
         ];
 
-        /** Jika tidak ada maka buat baru */
-        if (!$search) {
-            
-            $temp = new Temp;
+        /** jika qty 0, hapus row */
+        if ($data['quantity'] == 0) {
+            $temp = Temp::findOrFail($search['rowid']);
+            $temp->delete();
+        } else {
+            /** Jika tidak ada maka buat baru */
+            $temp = $search ? Temp::findOrFail($search['rowid']) : new Temp;
             $temp->user_id = auth()->user()->id;
             $temp->name = $temp_key;
             $temp->keys = serialize($data);
             $temp->save();
         }
+        
+        $search = TempDataHelper::searchKeyValue($temp_key, auth()->user()->id, ['item_id'], [$id]);
 
-        dd($temp);
+        /** get semua item */
+        $data = TempDataHelper::get($temp_key, auth()->user()->id);
+        $total_item = count($data);
+        $total_price = 0;
+        foreach ($data as $key => $value) {
+            $quantity = $value['quantity'];
+            $price = $quantity * $value['selling_price_item'];
+            $total_price += $price;
+        }
+        $search['total_item'] = $total_item;
+        $search['total_price'] = format_price($total_price);
+        return $search;
 
     }
 }
--- resources/views/frontend/c/pos/index.blade.php
+++ resources/views/frontend/c/pos/index.blade.php
@@ -14,90 +14,54 @@
     
     <!-- /Title -->
 
-    <!-- Right Setting Menu -->
-    <div class="setting-panel">
-        <ul class="right-sidebar nicescroll-bar pa-0">
-            <li class="layout-switcher-wrap">
-                <ul>
-                    <li>
-                        <span class="layout-title">PESANAN (20)</span>
-                        {{-- <span class="layout-switcher">
-                            <input type="checkbox" id="switch_3" class="js-switch"  data-color="#ea6c41" data-secondary-color="#878787" data-size="small"/>
-                        </span>	 --}}
-                        {{-- <h6 class="mt-30 mb-15">Theme colors</h6>
-                        <ul class="theme-option-wrap">
-                            <li id="theme-1" class="active-theme"><i class="zmdi zmdi-check"></i></li>
-                            <li id="theme-2"><i class="zmdi zmdi-check"></i></li>
-                            <li id="theme-3"><i class="zmdi zmdi-check"></i></li>
-                            <li id="theme-4"><i class="zmdi zmdi-check"></i></li>
-                            <li id="theme-5"><i class="zmdi zmdi-check"></i></li>
-                            <li id="theme-6"><i class="zmdi zmdi-check"></i></li>
-                        </ul> --}}
-                        <div class="chatapp-nicescroll-bar" style="overflow-y: scroll; width: auto; height: 443px;">
-                            <ul class="chat-list-wrap">
-                                <li class="chat-list">
-                                    <div class="chat-body">
-                                        @for ($i = 0; $i < 100; $i++)
-                                            
-                                        <a href="javascript:void(0)">
-                                            <div class="chat-data">
-                                                <img class="user-img img-circle" src="https://doyanresep.com/wp-content/uploads/2018/12/cara-membuat-nasi-goreng-telur.jpg" alt="user">
-                                                <div class="user-data">
-                                                    <span class="name block capitalize-font">Clay Masse</span>
-                                                    <span class="time block truncate txt-grey">3,000</span>
-                                                    <span class="">
-                                                        <label style="font-weight:bold" class="btn btn-xs btn-warning">-</label>
-                                                        <label style="font-weight:bold" class="btn btn-xs btn-default">2</label>
-                                                        <label style="font-weight:bold" class="btn btn-xs btn-warning">+</label>
-                                                    </span>
-                                                </div>
-                                                <div class="status away"></div>
-                                                <div class="clearfix"></div>
-                                            </div>
-                                        </a>
-                                        @endfor
-
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div style="font-weight:bold" class="row">
-                            <div class="pull-left col-lg-6 col-md-6 col-sm-6 col-xs-6">Total bayar</div>
-                            <div class="pull-right col-lg-6 col-md-6 col-sm-6 col-xs-6"> Rp. 34,000</div>
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <button id="reset_setting" class="btn btn-info btn-block btn-sm btn-primary mb-10">bayar sekarang</button>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-    <!-- /Right setting menu -->
-    
-    <button id="setting_panel_btn" class="btn btn-success btn-circle setting-panel-btn shadow-2dp"><i class="zmdi zmdi-shopping-basket"></i></button>
+    <button id="setting_panel_btn" class="btn btn-success setting-panel-btn shadow-2dp">
+        <i class="zmdi zmdi-shopping-basket"></i>
+        <div id="cart-item">
+            @if ($cart['total_item'] > 0)
+                {{$cart['total_item']}} items | Rp. {{$cart['total_price']}}
+            @endif
+        </div>
+    </button>
     <!-- Right Sidebar Backdrop -->
     <div class="right-sidebar-backdrop"></div>
     <!-- /Right Sidebar Backdrop -->
 
     <!-- Row -->
     <div class="row">
-        @foreach ($list_food as $item)
+        @foreach ($stand->slots as $item)
+        <?php
+            $temp_key = \App\Helpers\PosHelper::getTempKey();
+            $search = \App\Helpers\TempDataHelper::searchKeyValue($temp_key, auth()->user()->id, ['item_id'], [$item->id]);
+
+        ?>
         <div class="col-lg-2 col-md-4 col-sm-4 col-xs-6">
-            <div class="panel panel-default card-view pa-0">
+            <div class="panel panel-default card-view pa-5">
                 <div class="panel-wrapper collapse in">
-                    <div class="panel-body pa-0">
+                    <div class="panel-body pa-5">
                         <article class="col-item">
                             <div class="photo">
-                                <a href="javascript:void(0);"> <img src="{{asset($item->photo)}}" id="food-photo-{{$item->id}}" class="img-responsive" alt="Product Image" /> </a>
+                                <a href="javascript:void(0);"> <img src="{{asset($item->food->photo)}}" id="food-photo-{{$item->id}}" class="img-responsive" style="width:100%" alt="Product Image" /> </a>
                             </div>
-                            <div class="info">
-                                <div class="product-rating inline-block">
+                            <div class="pt-5">
+                                <div class="product-rating inline-block" style="font-size:12px">
                                     {{$item->vendingMachine ? $item->vendingMachine->name: null}}
                                 </div>
-                                <h6 id="food-name-{{$item->id}}">{{$item->food_name}}</h6>
-                                <span class="head-font block text-warning font-16" id="food-price-{{$item->id}}">{{format_quantity($item->selling_price_vending_machine)}}</span>
-                                <button class="btn btn-warning btn-sm" onclick="addToCart({{$item->id}})">Tambah</button>
+                                <h6 id="food-name-{{$item->id}}">{{$item->food->name}}</h6>
+                                <span class="head-font block text-warning font-16" id="food-price-{{$item->id}}">Rp. {{format_quantity($item->food->selling_price_vending_machine)}}</span>
+                                <br>
+                                <div id="btn-action-{{$item->id}}">
+                                    @if(!$search)
+                                        <button class="btn btn-warning btn-sm btn-block" onclick="addToCart({{$item->id}}, 0)">Tambah</button>
+                                    @else 
+                                    <div class="row text-centers" style="font-weight:bold; ">
+                                        <div style="font-weight:bold;" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <div style="border-radius:0px; font-size:14px; font-weight:bold; color:#f2b700 !important" class="col-xs-4 btn btn-default btn-xs" onclick="addToCart({{$item->id}}, 1)"><i class="fa fa-minus"></i></div>
+                                            <div style="border-radius:0px; font-size:14px; font-weight:bold; " class="col-xs-4 btn btn-default btn-xs">{{$search['quantity']}}</div>
+                                            <div style="border-radius:0px; font-size:14px; font-weight:bold; color:#f2b700 !important" class="col-xs-4 btn btn-default btn-xs" onclick="addToCart({{$item->id}}, 0)"><i class="fa fa-plus"></i></div>
+                                        </div>
+                                    </div>
+                                    @endif
+                                </div>
                             </div>
                         </article>
                     </div>
@@ -114,11 +78,34 @@
 <script>
     initDatatable('#datatable');
 
-    function addToCart(id) {
+    function addToCart(id, is_remove) {
         $.ajax({
-            url: '{{url("c/add-to-cart/")}}/'+id,
+            url: '{{url("c/add-to-cart/")}}/'+id+'?is_remove='+is_remove,
             success: function (res) {
-                notification('Item telah ditambahkan ke keranjang Anda');
+                if (res.quantity > 0) {
+                    var html = '<div class="row text-centers" style="font-weight:bold; ">'+
+                        '<div style="font-weight:bold;" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'+
+                            '<div style="border-radius:0px; font-size:14px; font-weight:bold; color:#f2b700 !important" class="col-xs-4 btn btn-default btn-xs" onclick="addToCart('+id+', 1)"><i class="fa fa-minus"></i></div>'+
+                            '<div style="border-radius:0px; font-size:14px; font-weight:bold;" class="col-xs-4 btn btn-default btn-xs">'+res.quantity+'</div>'+
+                            '<div style="border-radius:0px; font-size:14px; font-weight:bold; color:#f2b700 !important" class="col-xs-4 btn btn-default btn-xs" onclick="addToCart('+id+', 0)"><i class="fa fa-plus"></i></div>'+
+                        '</div>'+
+                    '</div>';
+                } else {
+                    var html = '<button class="btn btn-warning btn-sm btn-block" onclick="addToCart('+id+', 0)">Tambah</button>';
+                }
+
+                var cart = '';
+                if (res.total_item > 0) {
+                    var cart = res.total_item+' items | Rp. '+res.total_price;
+                }
+
+                $('#btn-action-'+id).html(html);
+                $('#cart-item').html(cart);
+                if (is_remove) {
+                    notification('Berhasil', 'Jumlah berhasil dikurangi');
+                } else {
+                    notification('Berhasil', 'Item telah ditambahkan ke keranjang Anda');
+                }
             }
         })
     }
