775087b0c49f026f1efb8db54b0c70115933789c
pringgojs <odyinggo@gmail.com>
2020-01-11 15:10:19 +0700

    update grafik dashboard

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -16,15 +16,18 @@ class FrontendController extends Controller
 {
     public function index(Request $request)
     {
+        $type = \Input::get('type');
         $view = view('frontend.dashboard.index');
-        $view->total_transaction = VendingMachineTransaction::search()->where('client_id', client()->id)->count();
+        $view->total_transaction = VendingMachineTransaction::joinVendingMachine()->where('vending_machines.type', $type)
+            ->select('vending_machine_transactions.*')->search()->where('vending_machine_transactions.client_id', client()->id)->count();
         $view->total_customer = Customer::count();
         $view->year = date('Y');
         $view->month = date('m');
-        $view->graph_transaction = GrafikHelper::grafikTransaction(date('Y'), date('m'));
-        $view->total_vending_machine = VendingMachine::where('client_id', client()->id)->count();
-        $view->total_profit = VendingMachineTransaction::search()->where('client_id', client()->id)->sum('profit_client');
-        $view->url_ajax_call = url('front/load-grafik-transaction');
+        $view->graph_transaction = GrafikHelper::grafikTransaction(date('Y'), date('m'), $type);
+        $view->total_vending_machine = VendingMachine::where('type', $type)->where('client_id', client()->id)->count();
+        $view->total_profit = VendingMachineTransaction::joinVendingMachine()->where('vending_machines.type', $type)
+            ->select('vending_machine_transactions.*')->search()->where('vending_machine_transactions.client_id', client()->id)->sum('profit_client');
+        $view->url_ajax_call = url('front/load-grafik-transaction?type='.$type);
 
         return $view;
     }
@@ -33,11 +36,12 @@ class FrontendController extends Controller
     {
         $year = \Input::get('year');
         $month = \Input::get('month');
+        $type = \Input::get('type');
         $view = view('frontend.dashboard._chart-morris');
-        $view->graph_transaction = GrafikHelper::grafikTransaction($year, $month);
+        $view->graph_transaction = GrafikHelper::grafikTransaction($year, $month, $type);
         $view->year = $year;
         $view->month = $month;
-        $view->url_ajax_call = url('front/load-grafik-transaction');
+        $view->url_ajax_call = url('front/load-grafik-transaction?type='.$type);
 
         return $view;
     }
