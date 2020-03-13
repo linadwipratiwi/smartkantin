dee835fcf49bf5098b0db5f21b3070cff4f76f88
iqomchanif <chanif.qomaruddin@gmail.com>
2020-03-13 21:40:51 +0700

    oke

--- app/Midtrans/Midtrans.php
+++ app/Midtrans/Midtrans.php
@@ -135,6 +135,7 @@ class Midtrans
         $info = curl_getinfo($ch);
         // curl_close($ch);
 
+        return $info;
         if ($result === false) {
             throw new Exception('CURL Error: ' . curl_error($ch), curl_errno($ch));
         } else {
